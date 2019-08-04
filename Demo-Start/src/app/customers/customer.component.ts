5b33972e3183dd2845763394548f71f447b95a1e
Juan Vargas <jvargasc@gmail.com>
2019-08-04 11:54:45 -0400

    Building a Reactive Form

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 import { Customer } from './customer';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +9,45 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      //lastName: {value: 'n/a', disabled: true},
+      lastName: '',
+      email: '',
+      sendCatalog: true
+    }); 
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(): void {
+
+    /*
+    In case we want to update all values we use setValue. 
+    Otherwise we use patchValue
+    */
+
+    // this.customerForm.setValue({
+    //   firstName: 'Jack',
+    //   lastName: 'Harkness',
+    //   email: 'jack@torchwood.com',
+    //   sendCatalog: false
+    // });
+
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      sendCatalog: false
+    });
+
   }
 }
